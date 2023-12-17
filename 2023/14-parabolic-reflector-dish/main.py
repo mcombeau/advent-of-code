12cff24babdb30384aee41eedb00f381691f6fbc
mcombeau <mcombeau@student.42.fr>
2023-12-17 19:20:19 +0100

    setup day 14

---
+++ 2023/14-parabolic-reflector-dish/main.py
@@ -0,0 +1,50 @@
+import argparse
+
+Args = argparse.Namespace
+Parser = argparse.ArgumentParser
+Map = list[str]
+
+
+def parse_args() -> Args:
+    parser: Parser = Parser()
+    parser.add_argument("filename")
+    args: Args = parser.parse_args()
+
+    return args
+
+
+#       PART ONE
+
+
+def calculate_result_part_1(lines: list[str]) -> int:
+    result: int = 0
+
+    return result
+
+
+#       PART TWO
+
+
+def calculate_result_part_2(lines: list[str]) -> int:
+    result: int = 0
+    return result
+
+
+#       MAIN
+
+
+def main() -> None:
+    args: Args = parse_args()
+    with open(args.filename) as file:
+        lines: list[str] = file.readlines()
+
+    result: int = calculate_result_part_1(lines)
+    print(f"Part 1 Result: {result}")
+
+    result: int = calculate_result_part_2(lines)
+    print(f"Part 2 Result: {result}")
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
