edc296496f60f1ee7628312df144af30e4a4e7e8
mcombeau <mcombeau@student.42.fr>
2023-12-17 16:11:29 +0100

    finish day 12 part 1

--- 2023/12-hot-springs/main.py
+++ 2023/12-hot-springs/main.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import cache
 
 Args = argparse.Namespace
 Parser = argparse.ArgumentParser
@@ -15,8 +16,53 @@ def parse_args() -> Args:
 #       PART ONE
 
 
+def is_valid_group(record: str, group: int) -> bool:
+    if group > len(record):
+        return False
+
+    contains_no_operational_gear = "." not in record[:group]
+    not_followed_by_damaged_gear = group == len(record) or record[group] != "#"
+
+    return (
+        True if contains_no_operational_gear and not_followed_by_damaged_gear else False
+    )
+
+
+@cache
+def get_number_arrangements(record: str, groups: tuple[int, ...]) -> int:
+    if record == "":
+        return 1 if groups == () else 0
+
+    if groups == ():
+        return 1 if not "#" in record else 0
+
+    result: int = 0
+
+    if record[0] in ".?":
+        result += get_number_arrangements(record[1:], groups)
+
+    if record[0] in "#?":
+        if is_valid_group(record, groups[0]):
+            result += get_number_arrangements(record[groups[0] + 1 :], groups[1:])
+        else:
+            result += 0
+
+    return result
+
+
 def calculate_result_part_1(lines: list[str]) -> int:
-    return 0
+    total_arrangements: int = 0
+
+    for line in lines:
+        count: int = 0
+        record, groups = line.split()
+        groups = tuple(map(int, groups.split(",")))
+
+        count += get_number_arrangements(record, groups)
+        total_arrangements += count
+        # print(f"{record:<20} {f'{groups}':<20} {count:<5}")
+
+    return total_arrangements
 
 
 #       PART TWO
