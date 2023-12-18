f0ef80e13fddf070ae0099117761e57064b33263
mcombeau <mcombeau@student.42.fr>
2023-12-18 02:10:17 +0100

    finish day 15 part 2

--- 2023/15-lens-library/main.py
+++ 2023/15-lens-library/main.py
@@ -2,7 +2,7 @@ import argparse
 
 Args = argparse.Namespace
 Parser = argparse.ArgumentParser
-Grid = list[str]
+Box = list[tuple[str, int]]
 
 
 def parse_args() -> Args:
@@ -13,6 +13,17 @@ def parse_args() -> Args:
     return args
 
 
+def decode_value(code: str) -> int:
+    current_value = 0
+
+    for c in code:
+        current_value += ord(c)
+        current_value *= 17
+        current_value %= 256
+
+    return current_value
+
+
 #       PART ONE
 
 
@@ -20,13 +31,8 @@ def calculate_result_part_1(line: str) -> int:
     sequences: tuple[str, ...] = tuple(line.split(","))
     result: int = 0
 
-    for seq in sequences:
-        current_value = 0
-        for c in seq:
-            current_value += ord(c)
-            current_value *= 17
-            current_value %= 256
-        result += current_value
+    for sequence in sequences:
+        result += decode_value(sequence)
 
     return result
 
@@ -34,8 +40,53 @@ def calculate_result_part_1(line: str) -> int:
 #       PART TWO
 
 
+def get_lens_info(sequence: str) -> tuple[str, int, int]:
+    label: str = sequence.split("=")[0] if "=" in sequence else sequence[:-1]
+    focal_length: int = int(sequence.split("=")[-1]) if "=" in sequence else -1
+    box_index: int = decode_value(label)
+
+    return (label, focal_length, box_index)
+
+
+def add_lens_to_box(box: Box, label: str, focal_length: int) -> None:
+    replaced_lens: bool = False
+
+    for i, lens in enumerate(box):
+        if lens[0] == label:
+            box[i] = (label, focal_length)
+            replaced_lens = True
+            break
+
+    if replaced_lens == False:
+        box.append((label, focal_length))
+
+
+def remove_lens_from_box(box: Box, label: str) -> None:
+    for i, lens in enumerate(box):
+        if lens[0] == label:
+            box.pop(i)
+            break
+
+
 def calculate_result_part_2(line: str) -> int:
-    return 0
+    sequences: tuple[str, ...] = tuple(line.split(","))
+    boxes: list[Box] = [[] for _ in range(256)]
+
+    for sequence in sequences:
+        label, focal_length, box_index = get_lens_info(sequence)
+
+        if "-" in sequence:
+            remove_lens_from_box(boxes[box_index], label)
+
+        elif "=" in sequence:
+            add_lens_to_box(boxes[box_index], label, focal_length)
+
+    result: int = 0
+    for b, box in enumerate(boxes):
+        for l, lens in enumerate(box):
+            result += (b + 1) * (l + 1) * lens[-1]
+
+    return result
 
 
 #       MAIN
